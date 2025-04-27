0cd914746757352fbc59010bb0f359ec5328edc5
sapphiregaze <jasmine@sapphiregaze.dev>
2025-04-26 17:27:21 -0700

    Updated Favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
+import { useRouter } from "next/navigation";
+
 export default function HomePage() {
-  window.location.replace("/dashboard")
+  const router = useRouter();
+  router.push("/auth");
   return <main></main>;
 }
